050db2d2c5c2c919db6708a353b00512fd9878c4
Randy Groves <randomgrace@gmail.com>
2022-01-13 11:46:09 -0800

    First check in of 2022 code
    Installed the 2020.1.1 WPILib and imported GradleRIO changes, among
    others.

---
+++ src/main/java/com/spartronics4915/frc/subsystems/ExampleSubsystem.java
@@ -0,0 +1,58 @@
+package com.spartronics4915.frc.subsystems;
+
+import com.spartronics4915.frc.Constants;
+import com.spartronics4915.lib.subsystems.SpartronicsSubsystem;
+
+import edu.wpi.first.wpilibj.motorcontrol.Talon;
+
+/**
+ * Detailed description of ExampleSubsystem.
+ */
+public class ExampleSubsystem extends SpartronicsSubsystem
+{
+    private static ExampleSubsystem sInstance = null;
+    // The subsystem's hardware is defined here...
+    private static Talon mTestMotor;
+
+    /** Creates a new ExampleSubsystem. */
+    public ExampleSubsystem()
+    {
+        boolean success = true;
+        try
+        {
+            mTestMotor = new Talon(Constants.kTestMotorId);
+        }
+        catch (Exception exception)
+        {
+            logException("Could not construct hardware: ", exception);
+            success = false;
+        }
+        logInitialized(success);
+    }
+
+    public static ExampleSubsystem getInstance()
+    {
+        if (sInstance == null) {
+            sInstance = new ExampleSubsystem();
+        }
+        return sInstance;
+    }
+
+    // Subsystem methods - actions the robot can take - should be placed here.
+
+    public void startTestMotor(double speed) {
+        mTestMotor.set(speed);
+    }
+
+    public void stopTestMotor() {
+        mTestMotor.set(0.0);
+    }
+
+    /** This method will be called once per scheduler run. */
+    @Override
+    public void periodic() {}
+
+    /** This method will be called once per scheduler run during simulation. */
+    @Override
+    public void simulationPeriodic() {}
+}
